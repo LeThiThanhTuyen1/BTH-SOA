b1769b37af9e6c29e2798add52eda5ba037b044a
LeThiThanhTuyen1 <130636825+LeThiThanhTuyen1@users.noreply.github.com>
2024-12-12 22:02:02 +0700

    fix /greeating

--- sao/src/main/java/com/bth/sao/controller/HelloController.java
+++ sao/src/main/java/com/bth/sao/controller/HelloController.java
@@ -3,10 +3,7 @@ package com.bth.sao.controller;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.bth.sao.model.Greeting;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -15,11 +12,6 @@ public class HelloController {
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
-
-	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
-	}
 	
 	@GetMapping("/")
 	public String greet(HttpServletRequest request) {

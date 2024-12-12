10a2f3e78216400f3c243ec58986b19c65262d99
LeThiThanhTuyen1 <130636825+LeThiThanhTuyen1@users.noreply.github.com>
2024-12-12 11:13:01 +0700

    initial

---
+++ sao/src/main/java/com/bth/sao/SaoApplication.java
@@ -0,0 +1,13 @@
+package com.bth.sao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaoApplication.class, args);
+	}
+
+}
